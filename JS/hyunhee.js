087bd9fa664e4c79f6edd4016360452551c984be
cara <cara656513@gmail.com>
2024-10-03 16:17:33 +0900

    자기소개 수정

--- JS/hyunhee.js
+++ JS/hyunhee.js
@@ -1,19 +1,22 @@
 const infoItems = [
-  { name: "이름 : ", description: "안현희" },
-  { name: "나이 : ", description: "35" },
-  { name: "MBTI : ", description: "ENFJ" },
-  { name: "취미 : ", description: "걷기" },
-  { name: "각오 : ", description: "최선을 다하겠습니다." },
+  { name: "이름 ", description: "안현희" },
+  { name: "MBTI ", description: "ENFJ" },
+  { name: "장점 ", description: "적극적" },
+  { name: "협업 스타일 ", description: "최선을 다하겠습니다." },
+  { name: "좋아하는 것 ", description: "걷기" },
+  { name: "하고 싶은 말", description: "최선을 다하겠습니다. 최선을 다하겠습니다.최선을 다하겠습니다.최선을 다하겠습니다.최선을 다하겠습니다.최선을 다하겠습니다.최선을 다하겠습니다.최선을 다하겠습니다.최선을 다하겠습니다.최선을 다하겠습니다." },
 ];
 
 const infoSection = document.querySelector("#contents");
 
 infoItems.forEach((item) => {
   const infoDiv = document.createElement("div");
-  const infoName = document.createElement("span");
+  const infoName = document.createElement("p");
   const infoContent = document.createElement("span");
 
   infoDiv.classList.add("info_box");
+  infoName.classList.add("info_name");
+  infoContent.classList.add("info_con")
   infoName.innerText = item.name;
   infoContent.innerText = item.description;
   infoDiv.append(infoName);
