ad5b16bacc03859070476753ddfd6e58d61851bf
minjun <minjun23221@gmail.com>
2024-10-06 17:52:25 +0900

    스크롤 수정

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,46 @@ let typing = function () {
 };
 
 setInterval(typing, 200);
+
+const windowHeight = window.innerHeight;
+const home4EventHandler = ()=>{
+  const Text = document.querySelector('.cont-title');
+
+  if(Text.getBoundingClientRect().top < windowHeight-200){
+      setTimeout(()=>{
+          Text.style.animation = 'appear_from_bottom ease 1.5s'
+          Text.style.opacity = 1
+      },200) 
+      window.removeEventListener('scroll',home4EventHandler)
+  }
+}
+window.addEventListener('scroll',home4EventHandler)
+
+
+const home3EventHandler = ()=>{
+  const Card0 = document.querySelector('.card');
+
+  if(Card0.getBoundingClientRect().top < windowHeight-200){
+      setTimeout(()=>{
+          Card0.style.animation = 'appear_from_bottom ease 1.5s'
+          Card0.style.opacity = 1
+
+      },200) 
+      window.removeEventListener('scroll',home3EventHandler)
+  }
+}
+window.addEventListener('scroll',home3EventHandler)
+
+const home2EventHandler = ()=>{
+  const Card1 = document.querySelector('.card34');
+
+  if(Card1.getBoundingClientRect().top < windowHeight-200){
+      setTimeout(()=>{
+          Card1.style.animation = 'appear_from_bottom ease 1.5s'
+          Card1.style.opacity = 1
+
+      },200) 
+      window.removeEventListener('scroll',home3EventHandler)
+  }
+}
+window.addEventListener('scroll',home2EventHandler)
